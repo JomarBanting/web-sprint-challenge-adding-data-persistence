b9a15de8fba8cc8cb76a9c333a652999b8620761
Jomar Banting <shadowlover.05@gmail.com>
2023-11-19 08:09:01 -0500

    Finished the project

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,7 @@
 // build your `Task` model here
 const db = require("../../data/dbConfig");
 
-async function postTask(data){
+async function postTask(data) {
     const taskId = await db("tasks").insert(data)
     const taskData = await getTaskById(taskId);
     const result = {
@@ -9,38 +9,42 @@ async function postTask(data){
         task_description: taskData.task_description,
         task_notes: taskData.task_notes,
         task_completed: taskData.task_completed ? true : false,
-        project_name: taskData.project_name,
-        project_description: taskData.project_description
+        project_id: taskData.project_id
     }
     return result;
 }
 
-async function getTaskById(id){
-    const taskData = await db("tasks").where("tasks.task_id", id);
+async function getTaskById(id) {
+    const taskData = await db("tasks").join("projects").where("tasks.task_id", id);
     return taskData[0];
 }
 
-async function getTasks(){
-    const tasksData = await db("tasks as t")
-    .join("projects as p")
-    .select(
-        "t.task_id", 
-        "t.task_description",
-        "t.task_notes",
-        "t.task_completed",
-        "p.project_name",
-        "p.project_description"
+async function getTasks() {
+    const tasksData = await db("tasks")
+        .leftJoin("projects", "projects.project_id", "tasks.project_id")
+        .select(
+            "tasks.task_id",
+            "tasks.task_description",
+            "tasks.task_notes",
+            "tasks.task_completed",
+            "projects.project_name",
+            "projects.project_description",
+            "tasks.project_id"
         )
-    const result = {
-        task_id: tasksData[0].task_id,
-        task_description: tasksData[0].task_description,
-        task_notes: tasksData[0].task_notes,
-        task_completed: tasksData[0].task_completed ? true : false,
-        project_name: tasksData[0].project_name,
-        project_description: tasksData[0].project_description
-    }
-    return tasksData;
+    const result = tasksData.map(data => {
+        return data = {
+            task_id: data.task_id,
+            task_description: data.task_description,
+            task_notes: data.task_notes,
+            task_completed: data.task_completed ? true : false,
+            project_name: data.project_name,
+            project_description: data.project_description
+        }
+    })
+
+    return result;
 }
+
 
 module.exports = {
     postTask,
